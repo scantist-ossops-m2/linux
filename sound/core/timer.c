8529274748431a3c64fd5e1822496f05fcec296b
jiongle1 <jiongle@scantist.com>
2024-03-29 15:41:15 +0800

    commit patch 4030421

--- sound/core/timer.c
+++ sound/core/timer.c
@@ -1136,6 +1136,7 @@ static void snd_timer_user_ccallback(snd_timer_instance_t *timeri,
 		tu->tstamp = *tstamp;
 	if ((tu->filter & (1 << event)) == 0 || !tu->tread)
 		return;
+	memset(&r1, 0, sizeof(r1));
 	r1.event = event;
 	r1.tstamp = *tstamp;
 	r1.val = resolution;
