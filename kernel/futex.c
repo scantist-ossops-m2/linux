8d5445f30db37ee9c5dbd65ed07062713da0cd6c
jiongle1 <jiongle@scantist.com>
2024-03-30 16:24:13 +0800

    commit patch 7621783

--- kernel/futex.c
+++ kernel/futex.c
@@ -1836,6 +1836,9 @@ retry:
 	if (unlikely(ret != 0))
 		goto out_put_key1;
 
+	if (nr_wake < 0 || nr_requeue < 0)
+		return -EINVAL;
+
 	/*
 	 * The check above which compares uaddrs is not sufficient for
 	 * shared futexes. We need to compare the keys:
